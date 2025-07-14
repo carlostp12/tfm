c06cfd1d064e165faab689d4eeed5b1f35139a31
carlostp12 <carlostp12@gmail.com>
2025-07-14 18:40:01 +0200

    change coords

--- R/functions.r
+++ R/functions.r
@@ -1,6 +1,11 @@
 sessionInfo()
 #readr_2.1.5    dbscan_1.2.2   jsonlite_2.0.0
 
+if (!require('jsonlite')) install.packages('jsonlite')
+if (!require('dbscan')) install.packages('dbscan')
+library(dbscan)
+if (!require('readr')) install.packages('readr')
+library(readr)
 get_distance <- function(z){
   url <- paste ("http://www.mathstools.com:8080/math/servlet/CosmologyServlet?z=",z,"&q=distance", sep="")
   data <- fromJSON(url)
@@ -16,5 +21,41 @@ parseRANumeric <- function (hour, min=0, sec=0)
 
 parseDECNumeric <- function (degree, min=0, sec=0)
 {
-  return (sec/3600 + min/60 + degree);
+  suma = 1
+  if (degree< -0){
+    suma = -1
+  }
+  return ((suma)*sec/3600 + (suma)*min/60 + degree);
+}
+
+changeCoordsSpericalX <- function (r, zeta_ra, psi_dec)
+{
+  return (c(changeCoordsSpericalX(r, zeta_ra, psi_dec),
+            changeCoordsSpericalY(r, zeta_ra, psi_dec),
+            changeCoordsSpericalZ(r, zeta_ra, psi_dec)
+            ))
+}
+
+parseCoords2Numeric <- function (hour, min=0, sec=0, degree, m=0, s=0)
+{
+  return (c(parseRANumeric(hour, min, sec),
+            parseDECNumeric(degree, m, s)
+  ))
+}
+
+changeCoordsSpericalX <- function (r, zeta_ra, psi_dec)
+{
+  x= r*cos(zeta_ra) * sin(psi_dec)
+  return (x)
+}
+
+changeCoordsSpericalY <- function (r, zeta_ra, psi_dec)
+{
+  x= r * sin(zeta_ra) * sin(psi_dec)
+  return (y)
+}
+changeCoordsSpericalZ <- function (r, zeta_ra, psi_dec)
+{
+  x= r*cos(psi_dec)
+  return (z)
 }
