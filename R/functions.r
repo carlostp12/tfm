91318148c606f638fa9042e1170e3f3f51a4750b
carlostp12 <carlostp12@gmail.com>
2025-07-23 18:33:21 +0200

    r/functions: parse galactic coords

--- R/functions.r
+++ R/functions.r
@@ -10,9 +10,14 @@ library(readr)
 if (!require('rjson')) install.packages('rjson')
 library(rjson)
 
+if (!require('dplyr')) install.packages('dplyr')
+library(dplyr)
+
 if (!require('akima')) install.packages('akima')
 library(akima)
 
+if (!require('astrolibR')) install.packages("astrolibR")
+library(astrolibR)
 
 get_distance <- function(z){
   url <- paste ("http://www.mathstools.com:8080/math/servlet/CosmologyServlet?z=",z,"&q=distance", sep="")
@@ -57,6 +62,39 @@ changeCoordsSpericalX <- function (r, zeta_ra, psi_dec)
             ))
 }
 
+changeCoordsGalactic <- function (dech, decm, decs, rah, ram, ras)
+{
+  #a<-glactc(ten(19,50,47), ten(8,52,6), 2000, gl, gb, 1)
+  a<-glactc(ten(dech, decm, decs), ten(rah, ram, ras), 2000, gl, gb, 1)
+  #return (a[1]$gl, a[2]$gb)
+  return (a)
+}
+
+changeCoordsGalacticGB <- function (ra, dec)
+{
+  #a<-glactc(ten(19,50,47), ten(8,52,6), 2000, gl, gb, 1)
+  a<-glactc(ra, dec, 2000, gl, gb, 1)
+  #return (a[1]$gl, a[2]$gb)
+  return (a$gb)
+}
+
+changeCoordsGalacticGL <- function (ra, dec)
+{
+  #a<-glactc(ten(19,50,47), ten(8,52,6), 2000, gl, gb, 1)
+  a<-glactc(ra, dec, 2000, gl, gb, 1)
+  #return (a[1]$gl, a[2]$gb)
+  return (a$gl)
+}
+
+parseToGalactic <- function (dt_sample)
+{
+  a<- mapply (changeCoordsGalactic, dt_sample$Ded, dt_sample$Dem, 
+              dt_sample$des, dt_sample$RAh, dt_sample$Ram, dt_sample$Ras)[,1]
+  dt_sample$gl=a[1]$gl
+  dt_sample$gb=a[2]$gb
+  return (dt_sample)
+}
+
 parseCoords2Numeric <- function (hour, min=0, sec=0, degree, m=0, s=0)
 {
   return (c(parseRANumeric(hour, min, sec),
@@ -81,7 +119,16 @@ changeCoordsSpericalZ <- function (r, zeta_ra, psi_dec)
   return (z)
 }
 
+setwd('C:/Users/Carlos/OneDrive/data-science/TFM/tfm/data')
+dt <- read.csv('2dfgrs-title-dist.csv')
+# podemos tomar una muestra:
+dt_sample <-sample_n(dt, 10)
 
+
+"""
+plot(df$ra, df$dec)
+df[ df$ra<20,'ra']
+"""
 datad2$dec <- parseDECNumeric(datad2$Ded, datad2$Dem, datad2$Des)
 datad2$ra <- parseRANumeric(datad2$RAh, datad2$Ram, datad2$Ras)
 setwd('C:/Users/Carlos/OneDrive/data-science/TFM/tfm/data')
@@ -96,8 +143,10 @@ z <- datad2$dist
 im <- with(data,interp(x,y,z))
 with(im,image(x,y,z))
 
-#Objetivo mañana: Consultar el SDSS
-
 a<- datad2[,c('ra', 'dec')]
 res <- optics(a, minPts = 1000)
 res <- extractDBSCAN(res, eps_cl = 5)
+
+dt_sample$gl <- mapply(changeCoordsGalacticGL, dt_sample$ra, dt_sample$dec)
+dt_sample$gb <- mapply(changeCoordsGalacticGB, dt_sample$ra, dt_sample$dec)
+
