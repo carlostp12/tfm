6991eded3a5dfbff0c38305a05ad7052748a7292
carlostp12 <carlostp12@gmail.com>
2025-09-07 22:03:10 +0200

    R: scaled symmetric distance calculation2

--- R/sdistance.r
+++ R/sdistance.r
@@ -6,7 +6,9 @@ calculate_angle <- function(v1, v2) {
 	mod_v1 <- sqrt(sum(v1^2))
 	mod_v2 <- sqrt(sum(v2^2))
 	acos_angle <- dot_product / (mod_v1 * mod_v2)
-	acos(acos_angle)
+	a_cos <- acos(acos_angle)
+	#TODO see what happen with the acos and na's
+	ifelse(is.na(a_cos), a_cos, 1)
 }
 
 #######################################
@@ -19,35 +21,37 @@ traverse_distance <- function(v1, v2, distance) {
 #######################################
 # Calculate scaled distance symmetric way
 #######################################
-d_LOS_scaled_symmetric <- function(v1, v2, sfactor) {
+d_LOS_scaled_symmetric <- function(v1, v2) {
 	(d_LOS(v1, v2) + d_LOS(v2, v1)) / 2
 }
 
 #######################################
 # Calculate scaled distance between two given vectors
+# sfactor is global constant, f.e sfactor=0.2
 #######################################
-d_LOS <- function(v1, v2, sfactor) {
+d_LOS <- function(v1, v2) {
 	sfactor * sum((v1-v2)*v1) / sqrt(sum(v1^2))
 }
 #######################################
 # Calculate elongated distance
 # depends on s_factor (a factor, for example 0.2) and proper_distance
-# v1 = c(x, y, x, distance, s_factor)
+# v1 = c(x, y, x, distance)
+# sfactor must be a global variable
 #######################################
 s_distance <- function(v1, v2){
 	vector1 <- v1[1:3]
 	vector2 <- v2[1:3]
 	proper_distance <- v1[4]
-	sfactor <- v1[5]
+	
     sqrt (traverse_distance(vector1, vector2, proper_distance)^2 + 
-              d_LOS_scaled(vector1, vector2, sfactor)^2
+              d_LOS_scaled_symmetric(vector1, vector2)^2
 		)
 }
 
 #######################################
 # Get a dist object made up from s_distances
 # use a coordintates matrix as follows:
-#	a<- mm[,c('x', 'y', 'z', distance, s_factor)]
+#	a<- mm[,c('x', 'y', 'z', distance)]
 # The aim is to call optics with this object as fo
 #   aaa <- as.dist(matriz_distancias)
 #   res <- optics(aaa, minPts = 5)
