85416e86ae7b50818feaa7baa9a2f1f6a2e11ca5
carlostp12 <carlostp12@gmail.com>
2025-10-06 20:13:22 +0200

    latex: PEC1 first version

---
+++ latex/TFM/delete.cmd
@@ -0,0 +1,9 @@
+del .\*.aux
+del .\*.blg
+del .\*.bbl
+del .\*.lof
+del .\*.log
+del .\*.lot
+del .\*.out
+del .\*.pdf
+del .\*.toc
---
+++ latex/TFM/execute.cmd
@@ -0,0 +1,13 @@
+pdflatex TFM
+bibtex TFM
+pdflatex TFM
+pdflatex TFM
+
+del .\*.aux
+del .\*.blg
+del .\*.bbl
+del .\*.lof
+del .\*.log
+del .\*.lot
+del .\*.out
+del .\*.toc
