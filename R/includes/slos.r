8b43ec1a299a5d55c43ce97b54018688fbad4470
carlostp12 <carlostp12@gmail.com>
2025-12-04 17:32:21 +0100

    R: create htmls files

---
+++ R/includes/slos.r
@@ -0,0 +1,105 @@
+#######################################
+# Calculate angle between two points
+#######################################
+#nulos <- 0
+calculate_angle <- function(v1, v2) {
+	dot_product <- sum(v1 * v2)
+	mod_v1 <- sqrt(sum(v1^2))
+	mod_v2 <- sqrt(sum(v2^2))
+	acos_angle <- dot_product / (mod_v1 * mod_v2)
+	bool <- abs(acos_angle)  >1 | is.na(acos_angle)
+	if(bool == TRUE) {
+		0
+	}else{
+		acos(acos_angle)
+	}
+	
+}
+
+
+#######################################
+# Calculate traverse distance from proper distance
+# simple Angle X Distance
+#######################################
+traverse_distance <- function(v1, v2, distance) {
+	calculate_angle(v1, v2) * distance
+}
+
+
+#######################################
+# Calculate scaled distance symmetric way
+#######################################
+d_LOS_scaled_symmetric <- function(v1, v2, redshift1, redshift2) {
+	(d_LOS(v1, v2, redshift1) + d_LOS(v2, v1, redshift2)) / 2
+}
+
+
+#######################################
+# Calculate scaled distance between two given vectors
+# sfactor is global constant, f.e sfactor=0.2
+# This formulae comes from Hai-Xia-Ma:2025:
+# https://arxiv.org/abs/2405.09855
+#######################################
+d_LOS <- function(v1, v2, redshift) {
+  if (SLOS>0){
+    sfactor <- SLOS
+  }else{
+    if(redshift< 0.01){
+		  sfactor <- 0.6
+	  } else if(redshift< 0.04){
+  		sfactor <- 0.5
+	  } else if(redshift< 0.06){
+		  sfactor <- 0.4
+	  } else if(redshift< 0.08){
+		  sfactor <- 0.35
+	  }else if(redshift< 0.1){
+		  sfactor <- 0.3
+	  }else if(redshift< 0.12){
+		  sfactor <- 0.19
+	  }else if(redshift< 0.15){
+		  sfactor <- 0.08
+	  }else if(redshift< 0.2){
+		  sfactor <- 0.01
+	  }else {
+		  sfactor <- 0.2
+	  }
+  }
+	sfactor * sum((v1-v2)*v1) / sqrt(sum(v1^2))
+}
+
+
+#######################################
+# Calculate elongated distance
+# depends on sfactor (a factor, for example sfactor=0.2) and proper_distance
+# v1 = c(x, y, x, distance)
+# sfactor must be a global variable
+#######################################
+s_distance <- function(v1, v2){
+  vector1 <- v1[1:3]
+	vector2 <- v2[1:3]
+	proper_distance <- v1[4]
+	#proper_distance <- (v1[4] + v2[4])/2
+	redshift1 <- v1[5]
+	redshift2 <- v2[5]
+  
+    sqrt (traverse_distance(vector1, vector2, proper_distance)^2 + 
+          d_LOS_scaled_symmetric(vector1, vector2, redshift1, redshift2)^2
+		)
+}
+
+
+#######################################
+# Get a dist object made up from s_distances
+# use a coordintates matrix as follows:
+#	a<- mm[,c('x', 'y', 'z', 'dist', 'redshift')]
+#   matriz_distancias <- get_matrix_of_distances(a)
+# The aim is to call optics with this object as follows
+#   aaa <- as.dist(matriz_distancias)
+#   res <- optics(aaa, minPts = min_members)
+#######################################
+get_matrix_of_distances <- function(matrix_coordintates) {
+	row_list <- as.list(data.frame(t(matrix_coordintates)))
+	distances_matrix <- outer(row_list, row_list, FUN = Vectorize(s_distance))
+	#as.dist(distances_matrix) This is not needed anymore
+	distances_matrix
+}
